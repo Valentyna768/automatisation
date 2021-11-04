413ed35e4ad1f1f833c2439322f922231c1d3cae
Valentyna768 <85171952+Valentyna768@users.noreply.github.com>
2021-11-04 11:07:39 +0200

    Create Draft.js

---
+++ Draft.js
@@ -0,0 +1,10 @@
+describe('Redirect', () => {
+    beforeEach(function () {
+        browser.url('https://nederlandscasinos.net/');
+    });
+    it('click on top1 cart and redirect to brand page', async() => {
+        const top1cart = $('body > div > div > div > div.bfad93f > main > section:nth-child(1) > div.casino-cards-box.bfad7e8 > div > div:nth-child(1) > div');
+        top1cart.click()
+        await browser.newWindow('https://nederlandscasinos.net/to-casino/?id=6512&location=https://nederlandscasinos.net/&cid=1313767433.1636014210&element=cta_cart_block')
+    });
+});
