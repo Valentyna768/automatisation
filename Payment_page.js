a4f19fb517c5e14779be2a03e710f5195b7c0bba
Valentyna768 <85171952+Valentyna768@users.noreply.github.com>
2021-12-24 16:30:15 +0200

    Create Payment_page.js

---
+++ Payment_page.js
@@ -0,0 +1,37 @@
+describe('Payment Page', () => {
+    beforeEach(async function () {
+        browser.url('https://nederlandscasinos.net/payments/ideal-casino/');
+        browser.setWindowSize(1440, 1079);
+    });
+    //tests without checking header, footer and widgets//
+    // Test 1  Check displaying of braedcrumps//
+    it('Check displaying of breadcrumps', () => {
+        const breadcrumps = $('body > div > div:nth-child(1) > div > div.bfad93f > div > p > span > span');
+        expect(breadcrumps).toBeDisplayed()
+        const homeBc = $('body > div > div:nth-child(1) > div > div.bfad93f > div > p > span > span > a')
+        expect(homeBc).toHaveTextContaining('Online Casino')
+        expect(homeBc).toHaveAttr('href')
+        expect(homeBc).toHaveLink('https://nederlandscasinos.net/')
+        homeBc.click()
+        const pageBc = $('body > div > div:nth-child(1) > div > div.bfad93f > div > p > span > span > strong')
+        expect(pageBc).toBeDisplayed
+    });
+    //Test2 Check H1//
+    it('Check H1', async () => {
+        const h1 = $('body > div > div:nth-child(1) > div > div.bfad93f > main > div > h1')
+        expect(h1).toBeDisplayed
+    });
+    // Test 3  Check casinos logo//
+    it('Check casinos logo', async () => {
+        const imgBlock = $('body > div > div:nth-child(1) > div > div.bfad93f > main > div > div.bfad5c9')
+        expect(imgBlock).toBeDisplayed
+        const image = $('body > div > div:nth-child(1) > div > div.bfad93f > main > div > div.bfad5c9 > img')
+        expect(image).toBeDisplayed
+        expect(image).toHaveAttr('alt="iDEAL casino"')
+    });
+    // Test 3  Check description//
+    it('Check description', async () => {
+        const description = $('body > div > div:nth-child(1) > div > div.bfad93f > main > div > div.bfade4f > p')
+        expect(description).toBeDisplayed
+    });
+});
